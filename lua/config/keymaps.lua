8893c0e39cd8e2795c0b0c8a1ce8b594e5047710
Kristian <kristian.hannula@proton.me>
2024-03-27 14:55:42 +0200

    lazyvim conf

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps herhttps://www.lazyvim.org/keymaps
+
+-- -- Remap left movement to 'j'
+vim.api.nvim_set_keymap("n", "j", "h", { noremap = true })
+vim.api.nvim_set_keymap("n", "J", "H", { noremap = true })
+
+-- Remap down movement to 'k'
+vim.api.nvim_set_keymap("n", "k", "j", { noremap = true })
+vim.api.nvim_set_keymap("n", "K", "J", { noremap = true })
+
+-- Remap up movement to 'l'
+vim.api.nvim_set_keymap("n", "l", "k", { noremap = true })
+vim.api.nvim_set_keymap("n", "L", "K", { noremap = true })
+
+-- Remap right movement to 'ö'
+vim.api.nvim_set_keymap("n", "ö", "l", { noremap = true })
+vim.api.nvim_set_keymap("n", "Ö", "L", { noremap = true })
---
+++ lua/plugins/colorsheme.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      style = "storm",
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+  },
+}
